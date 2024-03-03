d08faa4cb9639739bed366496395751708173149
ikudinov <aviakia99@gmail.com>
2024-03-04 02:06:03 +0300

    finish building header

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,23 +1,18 @@
 import React from "react"
+import NavbarMenu from "../components/navbarMenu"
 
 const Header = () => {
 	return (
-		<div className="container m-auto flex flex-col w-4/5">
+		<div className="container sm:m-auto p-5 flex flex-col sm:w-4/5 ">
 			<div className="flex">
-				<img src="logo.jpg" alt="" className="h-fit self-center" />
-				<div className="border border-solid border-black m-3"></div>
-				<div className="text-3xl font-bold flex flex-col justify-center">
+				<img src="logo.jpg" alt="" className="h-fit self-center m-auto" />
+				<div className="md:block hidden border border-solid border-black m-3"></div>
+				<div className="md:flex hidden flex-col text-3xl font-bold justify-center">
 					<div>Государственная специальная центральная библиотека для слепых и слабовидящих</div>
 					<div>State Library for the Blind</div>
 				</div>
 			</div>
-			<div className="flex mt-2 p-1 border border-solid border-gray-600 rounded-md">
-				<div className="text-2xl p-2 hover:bg-blue-300 rounded-md">Главная</div>
-				<div className="text-2xl p-2 hover:bg-blue-300 rounded-md">Указатели</div>
-				<div className="text-2xl p-2 hover:bg-blue-300 rounded-md">О ресурсе</div>
-				<div className="text-2xl p-2 hover:bg-blue-300 rounded-md">Поиск</div>
-				<div className="text-2xl p-2 hover:bg-blue-300 rounded-md">Контактная информация</div>
-			</div>
+			<NavbarMenu />
 		</div>
 	)
 }
